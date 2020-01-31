2ef6423aa65f46dde40db178ddbda3a0d6fbe9c1
pangfumin <fumin.pang@ninebot.com>
2020-01-31 16:14:22 +0800

    add ceres realted

--- spline_vio/include/spline_vio/spline/dynamic_spline_projection_error.h
+++ spline_vio/include/spline_vio/spline/dynamic_spline_projection_error.h
@@ -140,25 +140,94 @@ class SplineProjectionFactor4: public ceres::SizedCostFunction<2,7,7,7,7,1,1> {
         }
 
         bool Evaluate(double const *const *parameters, double *residuals, double **jacobians) const {
-            if (jacobians == nullptr) {
-                return ceres::internal::VariadicEvaluate<
-                        SplineProjectionFactor4, double, 7, 7, 7, 7, 1,1,0,0,0,0>
-                ::Call(*this, parameters, residuals);
-            }
 
 
-            bool success =  ceres::internal::AutoDiff<SplineProjectionFactor4, double,
-                    7,7,7,7,1,1>::Differentiate(
-                    *this,
-                    parameters,
-                    2,
-                    residuals,
-                    jacobians);
-
-            return true;
+            return EvaluateWithMinimalJacobians(parameters,
+                                                residuals,
+                                                jacobians, NULL);
 
 
         }
+
+
+    bool EvaluateWithMinimalJacobians(double const *const *parameters,
+                                                          double *residuals,
+                                                          double **jacobians,
+                                                          double **jacobiansMinimal) const {
+
+        if (!jacobians) {
+            bool success = ceres::internal::VariadicEvaluate<
+                    SplineProjectionFactor4, double, 7, 7, 7, 7, 1,1,0,0,0,0>
+            ::Call(*this, parameters, residuals);
+
+
+            return success;
+        }
+
+
+        bool success =  ceres::internal::AutoDiff<SplineProjectionFactor4, double,
+                7,7,7,7,1,1>::Differentiate(
+                *this,
+                parameters,
+                2,
+                residuals,
+                jacobians);
+
+        if (success && jacobiansMinimal!= NULL) {
+            Pose<double> T0(parameters[0]);
+            Pose<double> T1(parameters[1]);
+            Pose<double> T2(parameters[2]);
+            Pose<double> T3(parameters[3]);
+            if( jacobiansMinimal[0] != NULL){
+                Eigen::Map<Eigen::Matrix<double,2,6,Eigen::RowMajor>> J0_minimal_map(jacobiansMinimal[0]);
+                Eigen::Matrix<double,7,6,Eigen::RowMajor> J_plus;
+                PoseLocalParameter::plusJacobian(T0.parameterPtr(),J_plus.data());
+                Eigen::Map<Eigen::Matrix<double,2,7,Eigen::RowMajor>> J0_map(jacobians[0]);
+                J0_minimal_map = J0_map*J_plus;
+            }
+
+            if( jacobiansMinimal[1] != NULL){
+                Eigen::Map<Eigen::Matrix<double,2,6,Eigen::RowMajor>> J1_minimal_map(jacobiansMinimal[1]);
+                Eigen::Matrix<double,7,6,Eigen::RowMajor> J_plus;
+                PoseLocalParameter::plusJacobian(T1.parameterPtr(),J_plus.data());
+                Eigen::Map<Eigen::Matrix<double,2,7,Eigen::RowMajor>> J1_map(jacobians[1]);
+                J1_minimal_map = J1_map*J_plus;
+            }
+
+            if( jacobiansMinimal[2] != NULL){
+
+                Eigen::Map<Eigen::Matrix<double,2,6,Eigen::RowMajor>> J2_minimal_map(jacobiansMinimal[2]);
+                Eigen::Matrix<double,7,6,Eigen::RowMajor> J_plus;
+                PoseLocalParameter::plusJacobian(T2.parameterPtr(),J_plus.data());
+                Eigen::Map<Eigen::Matrix<double,2,7,Eigen::RowMajor>> J2_map(jacobians[2]);
+                J2_minimal_map = J2_map*J_plus;
+            }
+
+            if( jacobiansMinimal[3] != NULL){
+
+                Eigen::Map<Eigen::Matrix<double,2,6,Eigen::RowMajor>> J3_minimal_map(jacobiansMinimal[3]);
+                Eigen::Matrix<double,7,6,Eigen::RowMajor> J_plus;
+                PoseLocalParameter::plusJacobian(T3.parameterPtr(),J_plus.data());
+                Eigen::Map<Eigen::Matrix<double,2,7,Eigen::RowMajor>> J3_map(jacobians[3]);
+                J3_minimal_map = J3_map*J_plus;
+            }
+
+            if( jacobiansMinimal[4] != NULL){
+                Eigen::Map<Eigen::Matrix<double,2,1>> J4_minimal_map(jacobiansMinimal[4]);
+                Eigen::Map<Eigen::Matrix<double,2,1>> J4_map(jacobians[4]);
+                J4_minimal_map = J4_map;
+            }
+
+            if( jacobiansMinimal[5] != NULL){
+                Eigen::Map<Eigen::Matrix<double,2,1>> J5_minimal_map(jacobiansMinimal[5]);
+                Eigen::Map<Eigen::Matrix<double,2,1>> J5_map(jacobians[5]);
+                J5_minimal_map = J5_map;
+            }
+
+        }
+
+        return true;
+    }
 
         std::shared_ptr<ProjectionBase> projection_base_;
         double t0_;
